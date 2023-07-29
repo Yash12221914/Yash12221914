3a5bc77dd85839c31f719450722907f77567e021
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2023-07-29 13:55:25 +0530

    Delete speakgoodbye.js

--- speakgoodbye.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	var byeSpeaker = {};
-	var speakWord = "Goodbye";
-	byeSpeaker.speak = function(name) {
-		console.log(speakWord + " " + name);
-	}
-	window.byeSpeaker = byeSpeaker;
-})();
